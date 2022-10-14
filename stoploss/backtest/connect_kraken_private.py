f3a76ef7da923f27043cad6d3cdb269af3603352
kleinfossil <kleinfossil@googlemail.com>
2022-10-14 09:59:06 +0200

    initial backtest setup

---
+++ stoploss/backtest/connect_kraken_private.py
@@ -0,0 +1,22 @@
+def get_account_balance(key_type):
+    # Get the current account balance
+    json_response = {}
+    return json_response
+
+
+def get_open_orders(key_type):
+    # Get open Orders
+    json_response = {}
+    return json_response
+
+
+def trade_add_order(trade_dict, key_type):
+    # Creates a new order on kraken
+    json_response = {}
+    return json_response, True
+
+
+def trade_edit_order(trade_dict, key_type):
+    # Edits a Order
+    json_response = {}
+    return json_response, True
---
+++ stoploss/backtest/connect_kraken_public.py
@@ -0,0 +1,16 @@
+def get_ohlc_json(pair, interval=1, since=0):
+    # Provides Open, High, Low, Close Data. See: https://docs.kraken.com/rest/#operation/getOHLCData
+    json_response = {}
+    return json_response
+
+
+def get_ticker(pair):
+    # Gets the latest ticket
+    json_response = {}
+    return json_response
+
+
+def get_asset_pairs(pair):
+    # Gets Kraken asset Pairs
+    json_response = {}
+    return json_response
